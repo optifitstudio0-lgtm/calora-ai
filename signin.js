af0bf6f9de543d6a32a0c99d0e1761a19da6bf22
optifitstudio0-lgtm <optifitstudio0@gmail.com>
2025-11-03 10:26:23 +0000

    Update code

--- signin.js
+++ signin.js
@@ -9,7 +9,7 @@ import Icon from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { supabase } from '../supabaseclient';
+import { supabase } from './supabaseclient';
 
 import * as WebBrowser from 'expo-web-browser';
 import { makeRedirectUri } from 'expo-auth-session';
